8fb015ba424575e10112bc593fd298f64bc47125
martin.liivak <martin.liivak@gmail.com>
2020-01-29 23:34:46 +0200

    Correct read video frames not to be mirrored

--- src/learning/training/training_file_reader.py
+++ src/learning/training/training_file_reader.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 import json
 from collections import namedtuple
+import ffmpeg
 
 
 def get_namedtuple_from_json_string(line):
@@ -15,15 +16,21 @@ class TrainingFileReader:
 
     def read_training_video(self, filename):
         cap = cv2.VideoCapture(self.path_to_training + filename)
+        rotation = self.__check_rotation(filename)
         training_images = []
 
         while True:
             result, frame = cap.read()
-            if result:
-                training_images.append(frame)
 
             if cv2.waitKey(1) & 0xFF == ord('q') or not result:
                 break
+
+            if rotation is not None:
+                frame = self.__correct_rotation(frame, rotation)
+            else:
+                frame = cv2.flip(frame, 1)
+
+            training_images.append(frame)
 
         cap.release()
         return np.array(training_images)
@@ -38,3 +45,22 @@ class TrainingFileReader:
 
     def read_telemetry_as_csv(self, filename):
         return pd.read_csv(self.path_to_training + filename)
+
+    def __correct_rotation(self, frame, rotation):
+        return cv2.rotate(frame, rotation)
+
+    # https://stackoverflow.com/questions/53097092/frame-from-video-is-upside-down-after-extracting/55747773#55747773
+    def __check_rotation(self, filename):
+        video_metadata = ffmpeg.probe(self.path_to_training + filename)
+
+        if 'tags' not in video_metadata['streams'][0]:
+            return None
+        if 'rotate' not in video_metadata['streams'][0]['tags']:
+            return None
+
+        if int(video_metadata['streams'][0]['tags']['rotate']) == 90:
+            return cv2.ROTATE_90_CLOCKWISE
+        elif int(video_metadata['streams'][0]['tags']['rotate']) == 180:
+            return cv2.ROTATE_180
+        elif int(video_metadata['streams'][0]['tags']['rotate']) == 270:
+            return cv2.ROTATE_90_COUNTERCLOCKWISE
